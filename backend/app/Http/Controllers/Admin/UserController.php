f1c08c8cc03362f4f9bf8206fbc607bbc1fa9e1f
Belasri Imad <imadbelasri@gmail.com>
2025-05-07 13:43:19 +0100

    Update UserController.php
    remove user profile image when deleting

--- backend/app/Http/Controllers/Admin/UserController.php
+++ backend/app/Http/Controllers/Admin/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class UserController extends Controller
 {
@@ -23,6 +24,11 @@ class UserController extends Controller
      */
     public function delete(User $user)
     {
+        //check if the old image exists and remove it
+        if(File::exists(public_path($request->user()->profile_image))) {
+            File::delete(public_path($request->user()->profile_image));
+        }
+        
         $user->delete();
 
         return redirect()->route('admin.users.index')->with([
